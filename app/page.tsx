28eab3189a73192f51ead395b754feea09869446
Nama Kamu <email@example.com>
2025-05-09 15:01:59 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+import Core from "@/components/Core";
+import Header from "@/components/Header";
+import Projects from "@/components/Projects";
+import Tech from "@/components/Tech";
+import Image from "next/image";
+import Contact from "@/components/Contact";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <div className="py-10"></div>
+      <Core />
+      <div className="py-10"></div>
+      <Tech />
+      <div className="py-10"></div>
+      <Projects />
+      <div className="py-10"></div>
+      <Contact />
+    </>
+  );
+}
